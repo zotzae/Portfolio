71067812140b14e244f4120bc57feefe40ca9010
Zojae031 <edgar3@naver.com>
2019-08-17 20:22:03 +0900

    main 화면 구성

--- app/src/main/java/zojae031/portfolio/MainActivity.kt
+++ app/src/main/java/zojae031/portfolio/MainActivity.kt
@@ -1,12 +1,17 @@
 package zojae031.portfolio
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Glide.with(this@MainActivity)
+            .load("https://avatars2.githubusercontent.com/u/31091115?s=400&u=6db0ee4cb9b8a82cef9fe3a4196b437dc537339d&v=4")
+            .centerCrop()
+            .into(imageView)
     }
 }
