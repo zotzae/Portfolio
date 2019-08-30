324d42928be3b96ebb8674177e873ebf02362b98
Zojae031 <edgar3@naver.com>
2019-08-30 17:54:37 +0900

    Tec issue parsing

--- app/src/main/java/zojae031/portfolio/data/dao/tec/TecEntity.kt
+++ app/src/main/java/zojae031/portfolio/data/dao/tec/TecEntity.kt
@@ -2,6 +2,7 @@ package zojae031.portfolio.data.dao.tec
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.JsonArray
 
 @Entity
-data class TecEntity(@PrimaryKey val name: String, val image: String, val source: String)
+data class TecEntity(@PrimaryKey val name: String, val image: String, val source: JsonArray)
--- app/src/main/java/zojae031/portfolio/tec/TecPresenter.kt
+++ app/src/main/java/zojae031/portfolio/tec/TecPresenter.kt
@@ -1,8 +1,10 @@
 package zojae031.portfolio.tec
 
 import android.util.Log
+import com.google.gson.JsonParser
 import io.reactivex.android.schedulers.AndroidSchedulers
 import zojae031.portfolio.data.Repository
+import zojae031.portfolio.data.dao.tec.TecEntity
 
 class TecPresenter(private val view: TecContract.View, private val repository: Repository) :
     TecContract.Presenter {
@@ -23,8 +25,13 @@ class TecPresenter(private val view: TecContract.View, private val repository: R
     override fun onResume() {
         repository.getTecData()
             .observeOn(AndroidSchedulers.mainThread())
+            .map { data ->
+                JsonParser().parse(data.trim()).asJsonObject.run {
+                    TecEntity(get("name").asString, get("image").asString, getAsJsonArray("source"))
+                }
+            }
             .subscribe { data ->
-                Log.e("result ", data)
+                Log.e("result ", data.toString())
             }
 
     }
