46b7c390fa65ad38cc5340a91abc88426a5809b8
Zojae031 <edgar3@naver.com>
2019-09-20 16:27:35 +0900

    Edit FragmentStatePagerAdapter

--- app/src/main/java/zojae031/portfolio/main/MainPagerAdapter.kt
+++ app/src/main/java/zojae031/portfolio/main/MainPagerAdapter.kt
@@ -2,15 +2,13 @@ package zojae031.portfolio.main
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
 import zojae031.portfolio.profile.ProfileFragment
 import zojae031.portfolio.project.ProjectFragment
 import zojae031.portfolio.tec.TecFragment
 
-class MainPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(
-    fm,
-    BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
-) {
+class MainPagerAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+
     private val fragments = listOf<Fragment>(
         ProfileFragment(),
         ProjectFragment(),
